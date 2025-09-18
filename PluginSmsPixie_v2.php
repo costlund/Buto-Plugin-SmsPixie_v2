5c1fcd40d9dda98ed3098c431915f03ad421671a
Claes Östlund <claes@stenbergit.se>
2025-09-18 16:02:59 +0200

    1.0.0

---
+++ PluginSmsPixie_v2.php
@@ -0,0 +1,82 @@
+<?php
+class PluginSmsPixie_v2{
+  public static function send($data){
+    wfPlugin::includeonce('wf/array');
+    /**
+     * If param country is set but empty we have to set it to a valid one.
+     */
+    if(!$data->get('country')){
+      $data->set('country', '46');
+    }
+    /**
+     * Defaults
+     */
+    $default = new PluginWfArray();
+    $default->set('country', '46');
+    $default->set('sender', '_sender');
+    $default->set('token', '_token');
+    $default->set('to', '_to');
+    $default->set('message', '_message');
+    $default->set('cc', array());
+    /**
+     * Merge defaults.
+     */
+    $default = new PluginWfArray(array_merge($default->get(), $data->get()));
+    /**
+     * phone clean
+     */
+    $default->set('to', PluginSmsPixie_v2::remove_first_zero($default->get('to')));
+    /**
+     * recipients
+     */
+    $recipients = new PluginWfArray();
+    $recipients->set(true, $default->get('country').$default->get('to'));
+    /**
+     * recipients, cc
+     */
+    foreach ($default->get('cc') as $key => $value) {
+      $value = PluginSmsPixie_v2::remove_first_zero($value);
+      $recipients->set(true, $default->get('country').$value);
+    }
+    /**
+     * send
+     */
+    wfPlugin::includeonce('server/json');
+    $server = new PluginServerJson();
+    $server->token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJwZXRyaUBtYWMuY29tIiwianRpIjoiODNhN2I4ZjktMDU1ZC00M2IyLTgwNWYtNTdlOTg5NDY0ZTQ2IiwiaWF0IjoxNzU4MTEwOTM4LCJpZCI6IjExNjEwMjU0IiwiQ29tcGFueUlkIjoiMTE2MTAyNTQiLCJJc1N1YmFjY291bnQiOiJGYWxzZSIsIklzQXBpIjoiVHJ1ZSIsIklzQWRtaW4iOiJGYWxzZSIsIm5iZiI6MTc1ODExMDkzOCwiZXhwIjoyNTM0MDIzMDA4MDAsImlzcyI6InNtc0FwaSIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA4NS8ifQ.8hw1FA3UBr9DS-QTy41iqogkTAdcOWlvkUQ8uGYhFA8';
+    $default->set('recipients', $recipients->get());
+    $response = $server->send('https://app.pixie.se/api/v2/sms', $default->get());
+    $default->set('response', $response);
+    /**
+     * Log
+     */
+    $settings = wfPlugin::getPluginSettings('sms/pixie_v2', true);
+    if($settings->get('data/log')){
+      $log = new PluginWfYml(wfGlobals::getAppDir().$settings->get('data/log'));
+      $default->set('date', date('Y-m-d H:i:s'));
+      $log->set('log/', $default->get());
+      $log->save();
+    }
+    /**
+     * 
+     */
+    return $default->get();
+  }
+  public static function remove_first_zero($phone){
+    if(wfPhpfunc::substr($phone, 0, 1) == '0'){
+      $phone = wfPhpfunc::substr($phone, 1);
+    }
+    return $phone;
+  }
+  public function widget_test($data){
+    $data = new PluginWfArray($data);
+    $data = new PluginWfArray($data->get('data'));
+    wfPlugin::includeonce('sms/pixie_v2');
+    $result = $this->send($data);
+    echo '<h1>Test widget</h1>';
+    echo '<h2>Widget data</h2>';
+    wfHelp::print($data);
+    echo '<h2>Result</h2>';
+    wfHelp::print($result);
+  }
+}
---
+++ README.md
@@ -0,0 +1,81 @@
+# Buto-Plugin-SmsPixie_v2
+
+<p>SMS using <a href="http://www.pixie.se" target="_blank">Pixie</a> service.</p>
+<ul>
+<li><a href="https://pixie.se/support#api">https://pixie.se/support#api</a></li>
+<li><a href="https://app.pixie.se/#settings/user">https://app.pixie.se/#settings/user</a> (token)</li>
+<li>This is for the new service of Pixie after 2022.</li>
+<li>If you are using the old service before 2022 one must migrate to new service.</li>
+</ul>
+
+<a name="key_0"></a>
+
+## Settings
+
+<pre><code>plugin:
+  sms:
+    pixie_v2:
+      data:
+        log: '/../buto_data/theme/[theme]/pixie_v2_log.yml'</code></pre>
+
+<a name="key_1"></a>
+
+## Usage
+
+<pre><code>$data = new PluginWfArray();</code></pre>
+<p>Identifying sender.
+Alphanumeric, can have max 11 characters, number max 15 characters.</p>
+<pre><code>$data-&gt;set('sender', 'My-Web-Site');</code></pre>
+<pre><code>$data-&gt;set('token', ' Token generated in pixie.se panel. ');</code></pre>
+<pre><code>$data-&gt;set('to', ' Mobile number. ');</code></pre>
+<p>cc param to send copies.</p>
+<pre><code>$data-&gt;set('cc', array(' cc Mobile numbers (Optional). '));</code></pre>
+<pre><code>$data-&gt;set('message', 'Message from my web site.');</code></pre>
+<pre><code>$data-&gt;set('country', ' Optional default 46. ');</code></pre>
+<pre><code>wfPlugin::includeonce('sms/pixie_v2');
+PluginSmsPixie_v2::send($data);</code></pre>
+
+<a name="key_2"></a>
+
+## Widgets
+
+
+
+<a name="key_2_0"></a>
+
+### test
+
+<p>Widget for testing purpose.</p>
+<pre><code>type: widget
+settings:
+  role:
+    item:
+      - webmaster
+data:
+  plugin: sms/pixie_v2
+  method: test
+  data:
+    sender: SMS-TEST
+    token: ' my token '
+    to: ' mobile number '
+    country: '46'
+    message: 'Testing SMS.'</code></pre>
+
+<a name="key_3"></a>
+
+## Methods
+
+
+
+<a name="key_3_0"></a>
+
+### send
+
+
+
+<a name="key_3_1"></a>
+
+### remove_first_zero
+
+
+
